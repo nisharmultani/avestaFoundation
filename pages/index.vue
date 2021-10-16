0ad5b4fab8ff12a20ef0166232f298b8af750843
nishar multani <nisharmultani7@gmail.com>
2021-10-16 21:37:41 +0530

    WIP: work in progress

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
     <Navbar />
+    <heroHeader />
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar.vue";
+import heroHeader from '@/components/heroHeader.vue';
 export default {
   components: {
     Navbar,
+    heroHeader,
   },
 };
 </script>
+   

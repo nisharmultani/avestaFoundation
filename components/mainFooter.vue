2af16b385c1e3b4b2b9f7097e9635874fac36ecb
nishar multani <nisharmultani7@gmail.com>
2021-10-21 09:14:49 +0530

    feat add footer

---
+++ components/mainFooter.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <footer class="footerBody">
+      <div class="container">
+        <div class="footerLogo">this is logo section</div>
+        <div class="culomcontent">
+          <div class="row">
+            <div class="col-sm-3">
+              <div class="message">
+                <span>Message</span>
+                <p>
+                  We have distributed food materials to needy during lockdown We
+                  have distributed food materials to needy during lockdown We
+                  have distributed food materials to needy during lockdown We
+                  have distributed food materials to needy during lockdown We
+                  have distributed food materials to needy during lockdown
+                </p>
+              </div>
+            </div>
+            <div class="col-sm-3 vl">
+              <span>welcome</span>
+              <li>About Us</li>
+              <li>Volunteer</li>
+              <li>Causes</li>
+              <li>Projects</li>
+              <li>Board of Director</li>
+              <li>Shop</li>
+            </div>
+            <div class="col-sm-3 vl">
+              <div class="socialIcon">
+                <span>Follow Us</span>
+                <div class="icon">thi is icon</div>
+              </div>
+            </div>
+            <div class="col-sm-3 vl">
+              <div class="address">
+                <span>Foundation Address</span>
+                <p>
+                  Avesta Foundation C- 202, Rushabh Apartments CHSL,Veera Desai
+                  Road, Andheri West, Mumbai-58 Mail: foundationavesta@gmail.com
+                  Mail: info@avestafoundation.org Tell: +91 8369159889 Tell: +91
+                  9920331007
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="footerbottom">
+          <span>manage & maintan By nishar multani</span>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+span {
+  font-size: 18px;
+  font-weight: bold;
+  color: white;
+}
+p {
+  font-size: 16px;
+  color: white;
+}
+.footerBody {
+  height: auto;
+  background: black;
+}
+.footerLogo {
+  border-bottom: 1px solid grey;
+}
+.vl {
+  margin-top: 40px;
+  border-left: 2px solid grey;
+  height: 150px;
+}
+.culomcontent {
+  margin-bottom: 30px;
+}
+.footerbottom {
+  border-top: 1px solid grey;
+  height: 50px;
+  padding: 10px;
+}
+</style>
